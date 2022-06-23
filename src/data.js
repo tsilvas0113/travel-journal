7dd48d54861ae218fdcab7f74de75042027d383d
Tristan <tsilvas0113@gmail.com>
2022-06-23 16:11:08 -0500

    Start to make object data for 'main' component

--- src/data.js
+++ src/data.js
@@ -1,6 +1,13 @@
 const data = [
     {
-
+        id: 1,
+        title: 'Frio River',
+        location: 'CONCAN, TX',
+        mapsLink: 'https://goo.gl/maps/oGNeiUhRJh9LfWHT7',
+        startDate: '06 July, 2022',
+        endDate: '09 July, 2022',
+        description: "The Frio River winds through the heart of the Texas Hill Country, running shallow and crystal clear with spring-fed waters. When you go for a dip in the Frio, you'll love the pretty views overhead, including the tall limestone bluffs, big cypress trees, and stunning birds.",
+        image: './images/frio-river.png'
     }
 ]
 
