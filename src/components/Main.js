fe6f0ad5bfe730a100762c7bf8c726b63a2f518f
Tristan <tsilvas0113@gmail.com>
2022-06-24 11:36:51 -0500

    Implement data onto 'main' component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-function Main () {
+function Main (props) {
     return (
-        <section>
-
+        <section className="hero">
+            <img className="hero-img" src={props.image} alt="location image" />
+            <div className="hero-text">
+                <div className="location">
+                    <img className="map-icon" src="./images/marker-icon.png" alt="map marker" />
+                    <span className="city">{props.location}</span>
+                    <a href={props.mapsLink}>View on Google Maps</a>
+                </div>
+                <div className="location-name">{props.title}</div>
+                <div className="date">{props.startDate} - {props.endDate}</div>
+                <div className="description">{props.description}</div>
+            </div>
         </section>
     )
 }
