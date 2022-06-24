621bb5fbb27168af25802320bbbc10b0f9a1984b
Tristan <tsilvas0113@gmail.com>
2022-06-24 11:05:36 -0500

    Complete object data

--- src/data.js
+++ src/data.js
@@ -8,6 +8,26 @@ const data = [
         endDate: '09 July, 2022',
         description: "The Frio River winds through the heart of the Texas Hill Country, running shallow and crystal clear with spring-fed waters. When you go for a dip in the Frio, you'll love the pretty views overhead, including the tall limestone bluffs, big cypress trees, and stunning birds.",
         image: './images/frio-river.png'
+    },
+    {
+        id: 2,
+        title: 'Germania Insurance Amphitheater',
+        location: 'DEL VALLE, TX',
+        mapsLink: 'https://goo.gl/maps/X24cfqf4HTdn8PNF9',
+        startDate: '14 Sept, 2022',
+        endDate: '16 Sept, 2022',
+        description: "Germania Insurance Amphitheater is Austin's new destination to see the brightest stars in a world-class, open-air amphitheater. The venue, which is situated within Circuit of The Americas’ 1,300-acre sports and entertainment complex, has room for up to 14,000 guests, offers reserved seating for 6,700, and features a spacious lawn perfect for enjoying unforgettable performances under the stars with family and friends.",
+        image: './images/germania-theater.png'
+    },
+    {
+        id: 3,
+        title: 'AT&T Stadium',
+        location: 'ARLINGTON, TX',
+        mapsLink: 'https://goo.gl/maps/oGNeiUhRJh9LfWHT7',
+        startDate: '22 Nov, 2022',
+        endDate: '26 Nov, 2022',
+        description: "AT&T Stadium, formerly Cowboys Stadium, is a retractable roof stadium in Arlington, Texas, United States. It serves as the home of the Dallas Cowboys of the National Football League and was completed on May 27, 2009. It is also the home of the Cotton Bowl Classic and the Big 12 Championship Game.",
+        image: './images/att-stadium.png'
     }
 ]
 
